877ab76f3df529f07a790c1608d305459f11a626
吴恒宇 <578060214@qq.com>
2022-12-13 17:18:12 +0800

    add file

---
+++ 内置函数/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/12/13 17:16
+# @Author  : Walter
+# @File    : __init__.py
+# @License : (C)Copyright Walter
+# @Desc    :
---
+++ 文件/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/12/13 16:39
+# @Author  : Walter
+# @File    : __init__.py.py
+# @License : (C)Copyright Walter
+# @Desc    :
